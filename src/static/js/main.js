75c27de65f1323aa1c12330d9c55957dd3a4c945
Alan Shaw <alan@freestyle-developments.co.uk>
2013-06-23 19:20:02 +0100

    News backend

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,6 +1,36 @@
+window.cr = {}
+
+cr.loadFeedsApi = function (cb) {
+  try {
+    google.load("feeds", "1")
+    google.setOnLoadCallback(function (evt) {
+      cb()
+    })
+  } catch (e) {
+    cb(e)
+  }
+}
+
+cr.getNewsFeed = function (num, cb) {
+  cr.loadFeedsApi(function (er) {
+    if (er) return cb(er)
+    var feed = new google.feeds.Feed("http://careresponse.tumblr.com/rss")
+    feed.setNumEntries(num)
+    feed.load(function (res) {
+      if (res.error) return cb(new Error(res.error.message))
+      cb(null, res.feed)
+    })
+  })
+}
+
+// Get the post ID from a tumblr link
+cr.getPostId = function (link) {
+  return link.replace(/[^0-9]/g, "")
+}
+
 $(document).ready(function () {
   
-  $('a[href^="#"]').on('click',function (e) {
+  $('a[href^="#"]').on("click", function (e) {
     e.preventDefault()
     var target = this.hash, $target = $(target)
     $('html, body').stop().animate({
@@ -10,5 +40,43 @@ $(document).ready(function () {
     })
   })
   
-  $('.validation-engine').validationEngine()
+  $(".validation-engine").validationEngine()
+})
+
+$("#home").each(function () {
+  cr.getNewsFeed(2, function (er, feed) {
+    if (er) return console.error('Failed to load news feed', er)
+    var container = $("#box-home-news ul")
+    $.each(feed.entries, function (i, entry) {
+      var link = $("<a/>").attr("href", "news.html#" + cr.getPostId(entry.link)).text(entry.title).append('<i class="icon-angle-right"></i>')
+        , li = $("<li/>").append(link)
+      container.append(li)
+    })
+  })
+})
+
+$("#news").each(function () {
+  cr.getNewsFeed(5, function (er, feed) {
+    if (er) return console.error('Failed to load news feed', er)
+    var container = $('#articles')
+    $.each(feed.entries, function (i, entry) {
+      var title = $("<h1/>").append(entry.title)
+        , article = $("<article/>").append(title).append(entry.content).attr('id', cr.getPostId(entry.link))
+      container.append(article)
+    })
+    window.location.hash = window.location.hash
+  })
+})
+
+$("#testimonials").each(function () {
+  cr.getNewsFeed(2, function (er, feed) {
+    if (er) return console.error('Failed to load news feed', er)
+    var container = $("#box-news ul")
+    $.each(feed.entries, function (i, entry) {
+      var link = $("<a/>").attr("href", "news.html#" + cr.getPostId(entry.link)).text('More')
+        , title = $('<h2/>').text(entry.title)
+        , li = $("<li/>").append(title).append(entry.contentSnippet + ' ').append(link)
+      container.append(li)
+    })
+  })
 })
