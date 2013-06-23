d2d4dd4cc65c4f68cf42d335c1db094315b4ac97
Alan Shaw <alan@freestyle-developments.co.uk>
2013-06-23 17:34:44 +0100

    Fix the contact form!

--- src/static/contact-send.php
+++ src/static/contact-send.php
@@ -13,10 +13,10 @@ if (empty($_POST['forename']) || empty($_POST['surname']) || empty($_POST['tel']
 $to = 'info@careresponse.co.uk';
 $subject = 'careresponse.co.uk contact form submission';
 $body = "First name: ${_POST['forename']}\n";
-$body = "Surname: ${_POST['surname']}\n";
-$body = "Phone: ${_POST['tel']}\n";
-$body = "Email: ${_POST['email']}\n";
-$body = "Message: ${_POST['msg']}\n";
+$body .= "Surname: ${_POST['surname']}\n";
+$body .= "Phone: ${_POST['tel']}\n";
+$body .= "Email: ${_POST['email']}\n";
+$body .= "Message: ${_POST['msg']}\n";
 
 if (mail($to, $subject, $body)) {
   header('Location: contact.html#sent');
